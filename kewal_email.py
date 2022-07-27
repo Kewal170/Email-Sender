8aff983a40ab4100f8548c3091a32da5d539e00a
Kewal Suthar <102758565+Kewal170@users.noreply.github.com>
2022-07-27 22:32:35 +0530

    Add files via upload

---
+++ kewal_email.py
@@ -0,0 +1,23 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = "Email from Python"
+body = "This is the test email from python."
+sender_email = "<> sender email <>"
+receiver_email = "<> receiver email <>"
+password = "<> Your password <>"
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending Email...")
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+print("Email Sent!")
